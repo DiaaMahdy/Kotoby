732b4744d83ed3671a199a98c2db1077ae21fefe
DiaaMAhdy <diaaalmahdy77@gmail.com>
2024-05-15 03:19:11 +0300

    handling exception

---
+++ lib/features/home/data/repos/home_repo_impl.dart
@@ -0,0 +1,36 @@
+import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
+import 'package:kotoby/core/errors/failures.dart';
+import 'package:kotoby/core/utils/api_service.dart';
+import 'package:kotoby/features/home/data/models/book_model/book_model.dart';
+import 'package:kotoby/features/home/data/repos/home_repo.dart';
+
+class HomeRepoImpl implements HomeRepo {
+  final ApiService apiService;
+
+  HomeRepoImpl(this.apiService);
+  @override
+  Future<Either<Failure, List<BookModel>>> fetchNewestBooks() async {
+    try {
+      var data = await apiService.get(
+          endPoint:
+              'volumes?Filtering=free-ebooks&Sorting=newest &q=subject:programming');
+      List<BookModel> books = [];
+      for (var item in data['items']) {
+        books.add(BookModel.fromJson(item));
+      }
+      return right(books);
+    } on Exception catch (e) {
+      if (e is DioException) {
+        return left(ServerFailure.fromDioException(e));
+      }
+      return left(ServerFailure(e.toString()));
+    }
+  }
+
+  @override
+  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks() {
+    // TODO: implement fetchFeaturedBooks
+    throw UnimplementedError();
+  }
+}
